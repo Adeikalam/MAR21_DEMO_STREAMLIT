d73bd4e8883b6069c93054175fb5aaa7b08fe014
Adeikalam <46447749+Adeikalam@users.noreply.github.com>
2021-05-27 19:02:22 +0200

    Import pandas + models + Nettoyage du dataset

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -6,6 +6,31 @@ Created on Thu May 27 18:42:23 2021
 """
 
 import streamlit as st
+import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 st.title("Démo Streamlit Mar21 DA DS")
+
+df = pd.read_csv("train.csv", index_col = 'PassengerId')
+
+st.write(df)
+
+X = df.drop('Survived', axis = 1)
+y = df['Survived']
+
+
+# Nettoyage
+
+X = X.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
+X['Sex'] = X['Sex'].apply(lambda x : x == 'female')
+X['Age'] = X['Age'].fillna(X['Age'].mode()[0])
+X['Embarked'] = X['Embarked'].fillna(X['Embarked'].mode()[0])
+
+st.write(X.isna().sum())
+
+
+
