28445a37d903a255888fca807313fbbe4779536c
Adeikalam <46447749+Adeikalam@users.noreply.github.com>
2021-05-27 19:36:31 +0200

    enregistrement des modèles + chargement dans modelisation

--- modelisation.py
+++ modelisation.py
@@ -8,28 +8,27 @@ Created on Thu May 27 19:19:03 2021
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+from joblib import dump, load
 
+import streamlit as st
+
+
+@st.cache
 def get_model(choix, X_train, y_train, X_test, y_test):
 
     options = ['Regression Logistique', 'KNN', 'Decision Tree']
     if choix == options[0]:
-        model = LogisticRegression()
-    
-        model.fit(X_train, y_train)
+        model = load("logreg.joblib")
         
         score = model.score(X_test, y_test)
         
     if choix == options[1]:
-        model = KNeighborsClassifier()
-    
-        model.fit(X_train, y_train)
+        model = load("knn.joblib")
         
         score = model.score(X_test, y_test)
         
     if choix == options[2]:
-        model = DecisionTreeClassifier()
-    
-        model.fit(X_train, y_train)
+        model = load("tree.joblib")
         
         score = model.score(X_test, y_test)
         
